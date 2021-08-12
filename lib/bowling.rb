5a1b34d1eb468e040cda36e4e4dd0fcd376a37e7
EC2 Default User <ec2-user@ip-172-31-25-1.us-west-2.compute.internal>
2021-08-12 07:55:08 +0000

    プログラムコードの実装

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,3 +1,16 @@
 # ボウリングのスコアを管理するクラス
 class Bowling
+    
+    def initialize
+        #スコアの合計
+        @total_score = 0
+    end
+    
+    def total_score
+        @total_score
+    end
+    
+    def add_score(pins)
+        @total_score += pins
+    end
 end
