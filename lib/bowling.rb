67c1db22adf241d7597824f85322e6def1eb3606
EC2 Default User <ec2-user@ip-172-31-25-1.us-west-2.compute.internal>
2021-08-14 14:25:06 +0000

    ストライクのスコア計算のプログラムコードを実装

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -17,8 +17,9 @@ class Bowling
     def add_score(pins)
         # 一時保存用のスコアに、倒したピンの数を追加する。
         @temp << pins
-        # 2投分のデータが入っていれば、1フレーム分のスコアとして全体に追加する。
-        if @temp.size == 2
+        # 2投分のデータが入っているか、1投目がストライクだったら、
+        # 1フレーム分のスコアとして全体に追加する。
+        if @temp.size == 2 || strike?(@temp)
             @scores << @temp
             @temp = []
         end
@@ -26,8 +27,17 @@ class Bowling
     
     def calc_score
         @scores.each.with_index(1) do |score, index|
+            # 最終フレーム以外でのストライクなら、スコアにボーナスを含めて合計する。
+            if strike?(score) && not_last_frame?(index)
+                # 次のフレームもストライクで、なおかつ最終フレーム以外なら、
+                # もう一つ次のフレームの一投目もボーナスの対象とする。
+                if strike?(@scores[index]) && not_last_frame?(index + 1)
+                    @total_score += 20 + @scores[index + 1].first
+                else
+                    @total_score += 10 + @scores[index].inject(:+)
+                end
             # 最終フレーム以外でのスペアなら、スコアにボーナスを含めて合計する。
-            if spare?(score) && not_last_frame?(index)
+            elsif spare?(score) && not_last_frame?(index)
                 @total_score += calc_spare_bonus(index)
             else
                 @total_score += score.inject(:+)
@@ -37,9 +47,14 @@ class Bowling
     
     private
     
+    # ストライクかどうか判定する
+    def strike?(score)
+        score.first == 10
+    end
+    
     # スペアかどうか判定する
     def spare?(score)
-        score.inject(:+) == 10 && score.first != 10
+        score.inject(:+) == 10 && !strike?(score)
     end
     
     # 最終フレーム以外かどうか判定する。
